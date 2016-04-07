23532b178695d075954f7086c92a897bfffc2cb3
Maxim Lanin <maxim.lanin@acronis.com>
2016-04-07 17:12:54 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+if (typeof App !== 'undefined') {
+  var registeredActions = App
+}
+
+var App = {
+  /**
+   * Handle already registered actions.
+   * 
+   * @param  {String} id   Element ID
+   * @param  {String} text Text to show
+   */
+  init: function (id, text) {
+    document.getElementById(id).innerHTML = text;
+  },
+
+  /**
+   * Implement array's push method to handle push calls.
+   *
+   * @param  {Array|Function} item Method call. [method_name, args...]
+   */
+  push: function (item) {
+    if (typeof item === 'function') {
+      item()
+    } else {
+      var functionName = item.shift()
+      App[functionName].apply(null, item)
+    }
+  },
+  /**
+   * Handle already registered actions.
+   * 
+   * @param  {Array} array History of calls
+   */
+  _processPushes: function (array) {
+    for (var i = 0; i < array.length; i++) {
+      App.push(array[i])
+    }
+  }
+} 
+
+if (typeof registeredActions !== 'undefined') {
+  App._processPushes(registeredActions)
+}
